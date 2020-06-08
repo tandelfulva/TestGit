1abb18f44eff5d3b6317bfb3c11bf6c9bb803591
tandelfulva <65395413+tandelfulva@users.noreply.github.com>
2020-06-08 15:07:21 +0530

    Add files via upload

---
+++ maths/add_test.go
@@ -0,0 +1,39 @@
+package maths
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	actual := add(20, 30)
+	var expected int64 = 50
+
+	if actual != expected {
+		t.Error("Actual is:", actual, "but expected is:", expected)
+	}
+}
+
+func TestAddBulk(t *testing.T) {
+	var list = []struct {
+		first    int
+		second   int
+		expected int64
+	}{
+		{
+			20, 30, 50,
+		},
+		{
+			-20, 30, 10,
+		},
+		{
+			-20, -30, -50,
+		},
+		{
+			20, -30, -10,
+		},
+	}
+	for _, str := range list {
+		if actual := add(str.first, str.second); actual != str.expected {
+			t.Error("Actual is:", actual, "but expected is:", str.expected)
+		}
+	}
+
+}
